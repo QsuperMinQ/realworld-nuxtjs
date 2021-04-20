9e7204c53d0543f15792f44c6545b07cf3a1ff02
limin <limin.shen@smartnews.com>
2021-04-20 18:35:09 +0800

    update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,67 +2,68 @@
  * Nuxt.js 配置文件
  */
 
-module.exports = {
+ module.exports = {
     router: {
-        linkActiveClass: 'active',
-        extendRoutes(routes, resolve) {
-            console.log('routes:', routes)
-            routes.splice(0);
-            routes.push(...[
-                {
-                    path: '/',
-                    component: resolve(__dirname, 'pages/layout/'),
-                    children: [
-                        {
-                            path: '',
-                            name: 'home',
-                            component: resolve(__dirname, 'pages/home/')
-                        },
-                        {
-                            path: '/login',
-                            name: 'login',
-                            component: resolve(__dirname, 'pages/login/')
-                        },
-                        {
-                            path: '/register',
-                            name: 'register',
-                            component: resolve(__dirname, 'pages/login/')
-                        },
-                        {
-                            path: '/profile/:username',
-                            name: 'profile',
-                            component: resolve(__dirname, 'pages/profile/')
-                        },
-                        {
-                            path: '/setting',
-                            name: 'setting',
-                            component: resolve(__dirname, 'pages/setting/')
-                        },
-                        {
-                            path: '/editor',
-                            name: 'editor',
-                            component: resolve(__dirname, 'pages/editor/')
-                        },
-                        {
-                            path: '/article/:slug',
-                            name: 'article',
-                            component: resolve(__dirname, 'pages/article/')
-                        },
-
-                    ]
-                }
-            ])
-        }
+      linkActiveClass: 'active',
+      // 自定义路由表规则
+      extendRoutes (routes, resolve) {
+        // 清除 Nuxt.js 基于 pages 目录默认生成的路由表规则
+        routes.splice(0)
+  
+        routes.push(...[
+          {
+            path: '/',
+            component: resolve(__dirname, 'pages/layout/'),
+            children: [
+              {
+                path: '', // 默认子路由
+                name: 'home',
+                component: resolve(__dirname, 'pages/home/')
+              },
+              {
+                path: '/login',
+                name: 'login',
+                component: resolve(__dirname, 'pages/login/')
+              },
+              {
+                path: '/register',
+                name: 'register',
+                component: resolve(__dirname, 'pages/login/')
+              },
+              {
+                path: '/profile/:username',
+                name: 'profile',
+                component: resolve(__dirname, 'pages/profile/')
+              },
+              {
+                path: '/setting',
+                name: 'setting',
+                component: resolve(__dirname, 'pages/setting/')
+              },
+              {
+                path: '/editor',
+                name: 'editor',
+                component: resolve(__dirname, 'pages/editor/')
+              },
+              {
+                path: '/article/:slug',
+                name: 'article',
+                component: resolve(__dirname, 'pages/article/')
+              }
+            ]
+          }
+        ])
+      }
     },
-
+  
     server: {
-        host: '0.0.0.0',
-        port: 3001
-      },
-    
-      // 注册插件
-      plugins: [
-        '~/plugins/request.js',
-        '~/plugins/dayjs.js'
-      ]
-}
+      host: '0.0.0.0',
+      port: 3001
+    },
+  
+    // 注册插件
+    plugins: [
+      '~/plugins/request.js',
+      '~/plugins/dayjs.js'
+    ]
+  }
